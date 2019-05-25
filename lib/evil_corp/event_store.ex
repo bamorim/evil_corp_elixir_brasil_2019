aeb89dbaf1064b0150a6edff8a953a5d1dc29b3a
Bernardo Amorim <contato@bamorim.com>
2019-05-25 06:17:35 -0300

    make eventstore to use event schema

--- lib/evil_corp/event_store.ex
+++ lib/evil_corp/event_store.ex
@@ -1,6 +1,18 @@
 defmodule EvilCorp.EventStore do
+  alias EvilCorp.{
+    EventStore.Event,
+    Repo
+  }
+
   def persist(event) do
+    event
+    |> Event.from_event()
+    |> Repo.insert!()
   end
+
   def all do
+    Event
+    |> Repo.all()
+    |> Enum.map(&Event.to_event/1)
   end
 end

5daa0a1cb3682a06441a83e94dc03a4f610e6282
Bernardo Amorim <contato@bamorim.com>
2019-05-25 06:09:45 -0300

    config oban

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,7 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :evil_corp, Oban,
+  repo: EvilCorp.Repo,
+  queues: [default: 10]

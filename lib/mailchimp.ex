b0c40ecdc15f83979ee24597d1ec6978558bded8
Bernardo Amorim <contato@bamorim.com>
2019-05-25 06:04:41 -0300

    pretend a costly operation

--- lib/mailchimp.ex
+++ lib/mailchimp.ex
@@ -5,6 +5,8 @@ defmodule Mailchimp do
 
   @spec add_to_list(String.t(), String.t()) :: any()
   def add_to_list(email, name) do
+    # Let's pretend it is a costly operation
+    Process.sleep(1000)
     Logger.info("Adding #{email} (#{name}) to marketing list")
   end
 end
